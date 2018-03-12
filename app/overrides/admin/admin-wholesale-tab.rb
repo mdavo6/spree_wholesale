dab487d4320d0be4f24089d1260ef04d26035c83
Michael Davidson <mdavo6@gmail.com>
2018-03-12 21:14:51 +1100

    Added missing translation

--- app/overrides/admin/admin-wholesale-tab.rb
+++
@@ -1,9 +0,0 @@
-# insert_after :admin_tabs,                 'admin/hooks/wholesale_tab'
-
-# Deface::Override.new(
-#   :virtual_path => "spree/layouts/admin",
-#   :name => "admin-wholesale-tab",
-#   :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-#   :partial => "spree/admin/hooks/wholesale_tab",
-#   :disabled => false
-# )
