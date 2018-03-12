219377ae6ec75889fdfcb9aee4c90afcbc10718a
Michael Davidson <mdavo6@gmail.com>
2018-03-12 21:21:45 +1100

    Removed search sidebar

--- app/overrides/admin/admin-orders-search.rb
+++
@@ -1,6 +0,0 @@
-#insert_after :admin_orders_index_search,  'admin/hooks/admin_orders_index_search'
-Deface::Override.new(:virtual_path => 'spree/admin/orders/index',
-:name => 'admin-orders-search',
-:insert_before => "[data-hook='admin_orders_index_search_buttons']",
-:partial => "spree/admin/hooks/admin_orders_index_search",
-:disabled => false)
