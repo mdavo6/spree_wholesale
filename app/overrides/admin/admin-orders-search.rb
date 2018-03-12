620255022f33168d1726ea229c7ebd130f78032c
Michael Davidson <mdavo6@gmail.com>
2018-03-12 21:46:04 +1100

    Remove sidebar

--- app/overrides/admin/admin-orders-search.rb
+++ app/overrides/admin/admin-orders-search.rb
@@ -1,7 +1,7 @@
 #insert_after :admin_orders_index_search,  'admin/hooks/admin_orders_index_search'
 
-# Deface::Override.new(:virtual_path => 'spree/admin/orders/index',
-# :name => 'admin-orders-search',
-# :insert_before => "[data-hook='admin_orders_index_search_buttons']",
-# :partial => "spree/admin/hooks/admin_orders_index_search",
-# :disabled => false)
+Deface::Override.new(:virtual_path => 'spree/admin/orders/index',
+:name => 'admin-orders-search',
+:insert_before => "[data-hook='admin_orders_index_search_buttons']",
+:partial => "spree/admin/hooks/admin_orders_index_search",
+:disabled => false)
