ae2d1b943cf3277eb25967d2b306c15c01215f74
Michael Davidson <mdavo6@gmail.com>
2018-03-15 10:39:40 +1100

    Added wholesaler registration route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Spree::Core::Engine.routes.draw do
 
   get "/wholesaler/states" => "spree/wholesaler_states#index"
 
+  get '/wholesaler/registration' => 'wholesaler#registration', :as => :wholesaler_registration
+  put '/wholesaler/registration' => 'wholesaler#update_registration', :as => :update_wholesaler_registration
+
   resources :wholesalers
 
   namespace(:admin) do
