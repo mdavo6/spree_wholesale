aec4a1c3a6f6a4590b8694e9103b5b0ff06476ba
Michael Davidson <mdavo6@gmail.com>
2018-03-14 13:31:18 +1100

    Removal of .token in order_decorator due to error

--- app/helpers/spree/core/controller_helpers/order_decorator.rb
+++ app/helpers/spree/core/controller_helpers/order_decorator.rb
@@ -1,28 +1,23 @@
 Spree::Core::ControllerHelpers::Order.module_eval  do
 
-  # The current incomplete order from the session for use in cart and during checkout
+  # The current incomplete order from the guest_token for use in cart and during checkout
   def current_order(options = {})
     options[:create_order_if_necessary] ||= false
-    options[:lock] ||= false
-    return @current_order if @current_order
-    if session[:order_id]
-      current_order = Spree::Order.includes(:adjustments).lock(options[:lock]).where(id: session[:order_id], currency: current_currency).first
-      @current_order = current_order unless current_order.try(:completed?)
-    end
 
-    if options[:create_order_if_necessary] and (@current_order.nil? or @current_order.completed?)
-      @current_order = Spree::Order.new(currency: current_currency)
+    return @current_order if @current_order
+
+    @current_order = find_order_by_token_or_user(options, true)
+
+    if options[:create_order_if_necessary] && (@current_order.nil? || @current_order.completed?)
+      @current_order = Spree::Order.new(current_order_params)
       @current_order.user ||= try_spree_current_user
+
       @current_order.wholesale = spree_current_user.wholesaler? if spree_current_user
       # See issue #3346 for reasons why this line is here
       @current_order.created_by ||= try_spree_current_user
       @current_order.save!
-
-      # make sure the user has permission to access the order (if they are a guest)
-      if try_spree_current_user.nil?
-        session[:access_token] = @current_order.token
-      end
     end
+
     if @current_order
       if spree_current_user
         if spree_current_user.wholesaler? && !@current_order.is_wholesale?
@@ -32,8 +27,8 @@ Spree::Core::ControllerHelpers::Order.module_eval  do
         end
       end
       @current_order.last_ip_address = ip_address
-      session[:order_id] = @current_order.id
       return @current_order
     end
   end
+
 end
