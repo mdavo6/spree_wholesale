46a202ffd67b5cb153a2e080c712e2d1876446df
Michael Davidson <mdavo6@gmail.com>
2018-06-25 15:15:38 +1000

    Remove zero quantity items from order once confirm step is reached

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,9 +1,17 @@
 Spree::CheckoutController.class_eval do
   before_filter :get_addresses
+  before_filter :remove_zero_line_items
 
   def get_addresses
     return unless spree_current_user && spree_current_user.wholesaler? && !spree_current_user.wholesaler.nil?
     @order.bill_address = spree_current_user.wholesaler.bill_address
     @order.ship_address = spree_current_user.wholesaler.ship_address
+  end
+
+  # Removes zero quantity items from order once order reaches confirm step
+  def remove_zero_line_items
+    if @order.is_wholesale? && @order.state == "confirm"
+      @order.line_items = @order.line_items.select { |li| li.quantity > 0 }
+    end
   end
 end
