e7e18e8c2887c36f7d6b461c56618fb6feeeab40
Michael Davidson <mdavo6@gmail.com>
2019-02-19 20:03:55 +1100

    Update class of RRP price

--- app/overrides/products-wholesale-pricing.rb
+++ app/overrides/products-wholesale-pricing.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   :virtual_path => 'spree/shared/_products',
-  :name => 'products-wholesale-price',
+  :name => 'products-wholesale-pricing',
   :replace => "[data-hook='products_list_item'] .price.selling",
   :partial => "spree/hooks/products_wholesale_pricing",
   :disabled => false
