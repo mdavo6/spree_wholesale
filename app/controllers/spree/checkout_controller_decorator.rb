835afe6f01229f23a9507bcc2b8974d5fd60a83c
Michael Davidson <mdavo6@gmail.com>
2020-06-18 12:17:25 +1000

    Added minimum order check

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,5 +1,6 @@
 Spree::CheckoutController.class_eval do
   before_filter :get_addresses
+  before_action :check_minimum_met
   before_filter :create_a_password
   before_filter :register_as_wholesaler
 
@@ -8,6 +9,17 @@ Spree::CheckoutController.class_eval do
     if spree_current_user.wholesaler.bill_address.present?
       @order.bill_address = spree_current_user.wholesaler.bill_address
       @order.ship_address = spree_current_user.wholesaler.ship_address
+    end
+  end
+
+  def check_minimum_met
+    return unless spree_current_user && spree_current_user.wholesaler_or_lead?
+    if current_currency == "EUR" && current_order.item_total < 200
+      flash[:danger] = Spree.t(:minimum_order_message_euros)
+      redirect_back_or_default(cart_path)
+    elsif current_currency != "EUR" && current_order.item_total < 250
+      flash[:danger] = Spree.t(:minimum_order_message)
+      redirect_back_or_default(cart_path)
     end
   end
 
