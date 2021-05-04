887517869908ff49aa26d6a592d8f9f9c13d95df
Michael Davidson <mdavo6@gmail.com>
2021-05-04 14:38:54 +1000

    Comment out OrderContentsDecorator

--- app/models/spree/order_contents_decorator_OLD.rb
+++ app/models/spree/order_contents_decorator_OLD.rb
@@ -1,23 +1,23 @@
-Spree::OrderContents.class_eval do
-
-  def add_to_line_item(variant, quantity, options = {})
-    line_item = grab_line_item_by_variant(variant, false, options)
-
-    if line_item
-      line_item.quantity += quantity.to_i
-      line_item.currency = currency unless currency.nil?
-    else
-      # Added wholesale option
-      opts = { currency: order.currency, wholesale: (order.is_wholesale_or_lead? && variant.is_wholesaleable?) }.
-                                          merge ActionController::Parameters.new(options).
-                                          permit(Spree::PermittedAttributes.line_item_attributes)
-      line_item = order.line_items.new(quantity: quantity,
-                                        variant: variant,
-                                        options: opts)
-    end
-    line_item.target_shipment = options[:shipment] if options.has_key? :shipment
-    line_item.save!
-    line_item
-  end
-
-end
+# Spree::OrderContents.class_eval do
+#
+#   def add_to_line_item(variant, quantity, options = {})
+#     line_item = grab_line_item_by_variant(variant, false, options)
+#
+#     if line_item
+#       line_item.quantity += quantity.to_i
+#       line_item.currency = currency unless currency.nil?
+#     else
+#       # Added wholesale option
+#       opts = { currency: order.currency, wholesale: (order.is_wholesale_or_lead? && variant.is_wholesaleable?) }.
+#                                           merge ActionController::Parameters.new(options).
+#                                           permit(Spree::PermittedAttributes.line_item_attributes)
+#       line_item = order.line_items.new(quantity: quantity,
+#                                         variant: variant,
+#                                         options: opts)
+#     end
+#     line_item.target_shipment = options[:shipment] if options.has_key? :shipment
+#     line_item.save!
+#     line_item
+#   end
+#
+# end
