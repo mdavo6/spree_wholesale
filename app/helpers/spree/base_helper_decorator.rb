7f74bd7a71e1a767ed28f94d1269f96999bc51a6
Michael Davidson <mdavo6@gmail.com>
2021-05-19 10:59:16 +1000

    Update base_helper_decorator syntax for module

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,18 +1,20 @@
 module Spree
   module BaseHelperDecorator
+    def self.prepended(base)
+      base.module_eval do
+        def display_price(product_or_variant, wholesale = false)
+          product_or_variant.
+            price_in(current_currency, wholesale).
+            display_price_including_vat_for(current_price_options).
+            to_html
+        end
 
-    def display_price(product_or_variant, wholesale = false)
-      product_or_variant.
-        price_in(current_currency, wholesale).
-        display_price_including_vat_for(current_price_options).
-        to_html
+        def schema_price(product_or_variant, wholesale = false)
+          product_or_variant.price_in(current_currency, wholesale).
+          amount.to_s
+        end
+      end
     end
-
-    def schema_price(product_or_variant, wholesale = false)
-      product_or_variant.price_in(current_currency, wholesale).
-      amount.to_s
-    end
-
   end
 end
 
