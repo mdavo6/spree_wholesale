3859cb41d7683e7de6e66258c40e3671b339c000
Michael Davidson <mdavo6@gmail.com>
2022-03-21 15:29:10 +1100

    Remove time_ago_in_words from export

--- app/controllers/spree/admin/wholesalers_controller.rb
+++ app/controllers/spree/admin/wholesalers_controller.rb
@@ -123,7 +123,7 @@ module Spree
             total_orders = wholesaler.user.order_count
             if total_orders > 0
               wholesaler_attrs['Date of last order'] = wholesaler.user.orders.complete.reverse_chronological.first.updated_at.strftime('%d/%m/%Y')
-              wholesaler_attrs['Days since last order'] = (Date.today - time_ago_in_words(wholesaler.user.orders.reverse_chronological.first.updated_at.to_date)).to_i
+              wholesaler_attrs['Days since last order'] = (Date.today - wholesaler.user.orders.reverse_chronological.first.updated_at.to_date).to_i
             else
               wholesaler_attrs['Date of last order'] = 'N/A'
               wholesaler_attrs['Days since last order'] = 'N/A'
