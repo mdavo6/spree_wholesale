d7eac3ceaebd37bc706435c6470cc8a12aff733c
Michael Davidson <mdavo6@gmail.com>
2023-11-20 00:22:29 +1100

    Resolve issue with add to cart

--- app/helpers/spree/core/controller_helpers/order_decorator.rb
+++ app/helpers/spree/core/controller_helpers/order_decorator.rb
@@ -16,7 +16,7 @@ module Spree
           @current_order = find_order_by_token_or_user(options, true)
 
           if options[:create_order_if_necessary] && (@current_order.nil? || @current_order.completed?)
-            @current_order = Spree::Order.create!(current_order_params)
+            @current_order = current_store.orders.create!(current_order_params)
             @current_order.associate_user! try_spree_current_user if try_spree_current_user
             @current_order.last_ip_address = ip_address
 
