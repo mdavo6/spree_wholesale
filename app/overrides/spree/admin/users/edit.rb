0d60dd232ac93843b8e1ce3c03a96833c07e105c
Michael Davidson <mdavo6@gmail.com>
2023-11-28 10:23:37 +1100

    Added original to deface override

--- app/overrides/spree/admin/users/edit.rb
+++ app/overrides/spree/admin/users/edit.rb
@@ -3,5 +3,6 @@ Deface::Override.new(
   name: "login_link",
   insert_after: "[data-hook='admin_user_edit_general_settings']",
   partial: "spree/admin/users/login_link",
+  original: 'c295a231cb6869dea88c2a6bdf73e502ae3a338e',
   disabled: false
 )
