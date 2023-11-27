b25d1af477b17611c916011ba9772074858b1c1e
Michael Davidson <mdavo6@gmail.com>
2023-11-28 09:41:13 +1100

    Added disabled property to override

--- app/overrides/spree/admin/users/edit.rb
+++ app/overrides/spree/admin/users/edit.rb
@@ -2,4 +2,6 @@ Deface::Override.new(
   virtual_path: "spree/admin/users/edit",
   name: "login_link",
   insert_after: "[data-hook='admin_user_edit_general_settings']",
-  partial: "spree/admin/users/login_link")
+  partial: "spree/admin/users/login_link",
+  disabled: false
+)
